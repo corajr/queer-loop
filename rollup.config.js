537c8c285d91f125839790b0cb144c7c7d003156
Cora Johnson-Roberson <cora.johnson.roberson@gmail.com>
2019-05-23 12:51:32 -0400

    get rid of livereload

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import resolve from 'rollup-plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
-import livereload from 'rollup-plugin-livereload';
 import serve from 'rollup-plugin-serve';
 
 const production = !process.env.ROLLUP_WATCH;
@@ -16,7 +15,6 @@ export default [{
     plugins: [
         resolve(),
         !production && serve(),
-        !production && livereload(),
         production && terser()
     ],
 },
